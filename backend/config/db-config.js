dd19d68fa261ea086feb0bb5a54e36d31e6fd0e3
Ville Virtanen <cvivi007@edu.xamk.fi>
2022-02-26 23:27:16 +0200

    dockerfile corrections

--- backend/config/db-config.js
+++ backend/config/db-config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 // var connectionString = "postgres://userName:password@serverName/ip:port/nameOfDatabase";
 // const db = new Sequelize(connectionString);
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL_LOCALHOST);
 module.exports = db;
